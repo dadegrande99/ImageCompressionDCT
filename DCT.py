365da4636e43c6235e207ff4507a4c0f963b2664
fabbio00 <marinifabio000@gmail.com>
2023-06-24 17:08:24 +0200

    idct2 custom

--- DCT.py
+++ DCT.py
@@ -31,3 +31,31 @@ def custom_dct2(matrix):
                     sum += pixel_value * cosine_u * cosine_v
             dct_matrix[i, j] = sum * coeff_u * coeff_v / np.sqrt(rows*cols)
     return dct_matrix
+
+def custom_idct2(dct_image):
+   # Ottieni le dimensioni del blocco
+    M, N = dct_image.shape
+
+    # Inizializza l'array per il blocco di output
+    f = np.zeros((M, N), dtype=np.float64)
+
+    # Calcola la DCT2 inversa per ogni punto del blocco
+    for i in range(M):
+        for j in range(N):
+            # Calcola il valore del punto (i, j) del blocco di output
+            value = 0.0
+            for x in range(M):
+                for y in range(N):
+                    # Calcola il coefficiente di coseno
+                    coef = np.cos((np.pi / M) * (i + 0.5) * x) * np.cos((np.pi / N) * (j + 0.5) * y)
+
+                    # Aggiungi il termine alla somma
+                    value += coef * dct_image[x, y]
+
+            # Moltiplica il valore per il fattore di normalizzazione
+            value *= (2 / M) * (2 / N)
+
+            # Assegna il valore al punto (i, j) del blocco di output
+            f[i, j] = value
+
+    return f
--- image_processing.py
+++ image_processing.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
+from DCT import custom_dct2, custom_idct2
 
 def process_image(image_path, F, d):
     #suddividere l’immagine in blocchi quadrati f di pixel di dimensioni F×F partendo in alto a sinistra, scartando gli avanzi;
     image = cv2.imread(image_path, 0)
     height, width = image.shape
-    block_dim = min(height, width)
+    block_dim = min(height, width) // F
     image_dim = block_dim*F
     image = image[:image_dim, :image_dim]
     
@@ -19,13 +20,15 @@ def process_image(image_path, F, d):
             block = blocks[i, j]
 
             # Applica la DCT2
-            c = cv2.dct(np.float32(block))
+            c = custom_dct2(np.float32(block))
+            #c= cv2.dct(np.float32(block))
 
             # Taglia le frequenze
             c = threshold_cutoff(c, d)
 
             # Applica l'inversa della DCT2
-            ff = cv2.idct(c)
+            ff = custom_idct2(c)
+            #ff= cv2.idct(c)
 
             # Arrotonda e normalizza i valori
             ff = np.round(ff)
