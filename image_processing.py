dd2ff4358b576aab4b9a9d65fb94a6474c30698d
dadegrande99 <davidegrandesso@gmail.com>
2023-06-24 17:40:10 +0200

    corrige dimension

--- image_processing.py
+++ image_processing.py
@@ -2,33 +2,34 @@ import numpy as np
 import cv2
 from DCT import custom_dct2, custom_idct2
 
+
 def process_image(image_path, F, d):
-    #suddividere l’immagine in blocchi quadrati f di pixel di dimensioni F×F partendo in alto a sinistra, scartando gli avanzi;
+    # suddividere l’immagine in blocchi quadrati f di pixel di dimensioni F×F partendo in alto a sinistra, scartando gli avanzi;
     image = cv2.imread(image_path, 0)
     height, width = image.shape
-    block_dim = min(height, width) // F
-    image_dim = block_dim*F
-    image = image[:image_dim, :image_dim]
-    
-    blocks = np.split(image, block_dim, axis=0)
-    blocks = [np.split(block, block_dim, axis=1) for block in blocks]
+    bw = width // F  # block width
+    bh = height // F  # block height
+    image = image[:bh*F, :bw*F]
+
+    blocks = np.split(image, bh, axis=0)
+    blocks = [np.split(block, bw, axis=1) for block in blocks]
     blocks = np.array(blocks)
 
     # Applica il processo a ciascun blocco
-    for i in range(block_dim):
-        for j in range(block_dim):
+    for i in range(bh):
+        for j in range(bw):
             block = blocks[i, j]
 
             # Applica la DCT2
-            c = custom_dct2(np.float32(block))
-            #c= cv2.dct(np.float32(block))
+            # c = custom_dct2(np.float32(block))
+            c = cv2.dct(np.float32(block))
 
             # Taglia le frequenze
             c = threshold_cutoff(c, d)
 
             # Applica l'inversa della DCT2
-            ff = custom_idct2(c)
-            #ff= cv2.idct(c)
+            # ff = custom_idct2(c)
+            ff = cv2.idct(c)
 
             # Arrotonda e normalizza i valori
             ff = np.round(ff)
@@ -38,11 +39,13 @@ def process_image(image_path, F, d):
             blocks[i, j] = ff
 
     # Ricompone l'immagine a partire dai blocchi
-    reconstructed_image = np.block([[block for block in row] for row in blocks])
+    reconstructed_image = np.block(
+        [[block for block in row] for row in blocks])
 
     # Converte l'immagine in formato byte
     reconstructed_image = reconstructed_image.astype(np.uint8)
     return reconstructed_image
+
 
 def threshold_cutoff(coefficients, threshold):
     size = coefficients.shape[0]
